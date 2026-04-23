7cf3b199c77050b5ba0323b6b976505524ad0a7d
NathanPham <npham45@gmail.com>
2026-04-23 18:10:50 -0500

    clean up

--- app/components/ui/BottomBar.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export function BottomBar({ children }: { children: React.ReactNode }) {
-  return (
-    <div className="sticky bottom-0 left-0 right-0 -mx-4 mt-6 border-t border-black/10 bg-white/80 px-4 py-3 backdrop-blur">
-      {children}
-    </div>
-  );
-}
--- app/components/ui/Container.tsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export function Container({ children }: { children: React.ReactNode }) {
-  return <div className="mx-auto w-full max-w-md px-4 py-5">{children}</div>;
-}
